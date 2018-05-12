6af74a552a52245543f29c7992703d7e643e56fd
MemoOR <dethkarz2009@hotmail.com>
2018-05-11 20:48:58 -0500

    iniciotab

--- Proyecto/iniciotab.c
+++ Proyecto/iniciotab.c
@@ -29,16 +29,19 @@ typedef struct _node2{
   char *tablero[20][20];
 }Lista;
 
-GtkWidget *AddButton(GtkWidget *theBox, const gchar *buttonText, gpointer CallBackFunction, Lista *EntryBox);
-GtkWidget *AddButton1(GtkWidget *window,GtkWidget *theBox, const gchar *buttonText, gpointer CallBackFunction, Lista *EntryBox);
+GtkWidget *AddButton(GtkWidget *theBox, const gchar *buttonText,
+		     gpointer CallBackFunction, Lista *EntryBox);
+GtkWidget *AddButton1(GtkWidget *window,GtkWidget *theBox,
+		      const gchar *buttonText, gpointer CallBackFunction,
+		      Lista *EntryBox);
+GtkWidget *Addlabel(GtkWidget *theBox, const gchar *buttonText);
 void StopTheApp(GtkWidget *window, gpointer data);
 void Nuevo(GtkWidget *window, gpointer data);
 void Cargar(GtkWidget *window, gpointer data);
 void NOMBRES(GtkWidget *window, gpointer data);
 void CARGAR(GtkWidget *window, gpointer data);
 void Cargarjugs(GtkWidget *window, gpointer data);
-static gboolean delete_event(GtkWidget *widget, GdkEvent *event, gpointer *data);
-static void destroy(GtkWidget *widget, gpointer data);
+
 void command(int y);
 void callback(GtkWidget *widget, gpointer callback_data);
 GtkWidget *create_pad();
@@ -79,25 +82,16 @@ gint main ( gint argc, gchar *argv[]){
   gtk_window_set_default_size(GTK_WINDOW(window1),320,200);
   gtk_container_border_width(GTK_CONTAINER(window1),25);
 
-
   verticalbox = gtk_vbox_new(TRUE,5);
-
   Inicio->ventana = window1;
-
   label = gtk_label_new ("¡Bienvenidos al juego de Pente!");
-
   gtk_box_pack_start(GTK_BOX(verticalbox),label,TRUE,TRUE,5);
   
-  
   box = gtk_hbox_new(TRUE,30);
-  
-  
   button = AddButton1(window1,box,"Juego Nuevo",Nuevo,Inicio);
   button = AddButton1(window1,box,"Cargar",Cargar,Inicio);
- 
   gtk_box_pack_start(GTK_BOX(verticalbox),box,TRUE,TRUE,5);
 
-  
   gtk_container_add(GTK_CONTAINER(window1),verticalbox);
   
   /*
@@ -106,32 +100,45 @@ gint main ( gint argc, gchar *argv[]){
   gtk_box_pack_start(GTK_BOX(verticalbox),entrybox,TRUE,TRUE,5);
   */
 
-  gtk_signal_connect(GTK_OBJECT(window1),"destroy",GTK_SIGNAL_FUNC(StopTheApp),NULL);
-
+  gtk_signal_connect(GTK_OBJECT(window1),
+		     "destroy",GTK_SIGNAL_FUNC(StopTheApp),NULL);
   gtk_widget_show_all(window1);
-  
   gtk_main();
   
   return 0;
 }
 
-GtkWidget *AddButton(GtkWidget *theBox, const gchar *buttonText, gpointer CallBackFunction, Lista *EntryBox){
+GtkWidget *AddButton(GtkWidget *theBox, const gchar *buttonText,
+		     gpointer CallBackFunction, Lista *EntryBox){
   GtkWidget *button;
   button = gtk_button_new_with_label(buttonText);
   gtk_box_pack_start(GTK_BOX(theBox),button,FALSE,TRUE,10);
-  gtk_signal_connect(GTK_OBJECT(button),"clicked",GTK_SIGNAL_FUNC(CallBackFunction),EntryBox);
+  gtk_signal_connect(GTK_OBJECT(button),"clicked",
+		     GTK_SIGNAL_FUNC(CallBackFunction),EntryBox);
   gtk_widget_show(button);
   return button;
 }
 
-GtkWidget *AddButton1(GtkWidget *window, GtkWidget *theBox, const gchar *buttonText, gpointer CallBackFunction, Lista *EntryBox){
+GtkWidget *AddButton1(GtkWidget *window, GtkWidget *theBox,
+		      const gchar *buttonText, gpointer CallBackFunction,
+		      Lista *EntryBox){
   GtkWidget *button;
   button = gtk_button_new_with_label(buttonText);
   gtk_box_pack_start(GTK_BOX(theBox),button,FALSE,TRUE,10);
-  gtk_signal_connect(GTK_OBJECT(button),"clicked",GTK_SIGNAL_FUNC(CallBackFunction),EntryBox);
-  g_signal_connect_swapped (G_OBJECT (button), "clicked", G_CALLBACK (gtk_widget_hide), G_OBJECT (window));
+  gtk_signal_connect(GTK_OBJECT(button),"clicked",
+		     GTK_SIGNAL_FUNC(CallBackFunction),EntryBox);
+  g_signal_connect_swapped (G_OBJECT (button), "clicked",
+			    G_CALLBACK (gtk_widget_hide), G_OBJECT (window));
   gtk_widget_show(button);
   return button;
+}
+
+GtkWidget *Addlabel(GtkWidget *theBox, const gchar *buttonText){
+    GtkWidget *label;
+    label = gtk_label_new(buttonText);
+    gtk_box_pack_start(GTK_BOX(theBox),label,FALSE,FALSE,0);
+    gtk_widget_show(label);
+    return label;
 }
 
 void close_window(GtkWidget *window, gpointer data){
@@ -152,46 +159,45 @@ void Nuevo(GtkWidget *window, gpointer data){
   windownombres = gtk_window_new (GTK_WINDOW_TOPLEVEL);
   gtk_window_set_default_size(GTK_WINDOW(windownombres),320,200);
   gtk_container_border_width(GTK_CONTAINER(windownombres),5);
-  
-  verticalbox = gtk_vbox_new(TRUE,5);
 
-  
-  label1 = gtk_label_new ("Jugador 1:");
-  gtk_box_pack_start(GTK_BOX(verticalbox),label1,TRUE,TRUE,5);
+  verticalbox = gtk_vbox_new(TRUE,5);
+  label1 = Addlabel(verticalbox,"Jugador 1:");
   
   entrybox = gtk_entry_new();
   Inicio->Jugador1->entry = entrybox;
-  
-  
   gtk_box_pack_start(GTK_BOX(verticalbox),entrybox,TRUE,TRUE,5);
     
-  label2 = gtk_label_new ("Jugador 2:");
-  gtk_box_pack_start(GTK_BOX(verticalbox),label2,TRUE,TRUE,5);
-
+  label2 = Addlabel(verticalbox,"Jugador 2:");
+  
   Entrybox = gtk_entry_new();
   Inicio->Jugador2->entry = Entrybox;
-    
   gtk_box_pack_start(GTK_BOX(verticalbox),Entrybox,TRUE,TRUE,5);
 
-  gtk_signal_connect(GTK_OBJECT(windownombres),"destroy",GTK_SIGNAL_FUNC(StopTheApp),NULL);
+  gtk_signal_connect(GTK_OBJECT(windownombres),"destroy"
+		     ,GTK_SIGNAL_FUNC(StopTheApp),NULL);
 
-  button = gtk_button_new_with_label("continuar");
+  
+  button = gtk_button_new_with_label("Continuar");
   gtk_box_pack_start(GTK_BOX(verticalbox),button,FALSE,TRUE,10);
-  gtk_signal_connect(GTK_OBJECT(button),"clicked",GTK_SIGNAL_FUNC(NOMBRES),Inicio);
-  g_signal_connect_swapped (G_OBJECT (button), "clicked", G_CALLBACK (gtk_widget_hide), G_OBJECT (windownombres));
+  g_signal_connect_swapped (G_OBJECT (button), "clicked",
+			    G_CALLBACK (gtk_widget_hide), G_OBJECT (windownombres));
+  gtk_signal_connect(GTK_OBJECT(button),"clicked",
+		     GTK_SIGNAL_FUNC(NOMBRES),Inicio);
   gtk_widget_show(button);
-
+  
+  //button = AddButton(verticalbox,"Continuar",NOMBRES,Inicio);
+ 
+  Inicio->ventana = windownombres;
   
   //button = AddButton(verticalbox,"Continuar",NOMBRES,Inicio);
   
   //Cuando se apriete el boton, que guarde entrybox y Entrybox en Lista->Jug1 y Lista->Jug2
 
   gtk_container_add(GTK_CONTAINER(windownombres),verticalbox);
-
   gtk_widget_show_all(windownombres);
-  
-  /*********************************************************/
 }
+
+/*********************************************************/
 
 void Cargar(GtkWidget *window, gpointer data){
 
@@ -207,30 +213,23 @@ void Cargar(GtkWidget *window, gpointer data){
   gtk_container_border_width(GTK_CONTAINER(windownombres),5);
   
   verticalbox = gtk_vbox_new(TRUE,5);
-
-  label1 = gtk_label_new ("Escriba el nombre del archivo que quiere cargar:");
-  gtk_box_pack_start(GTK_BOX(verticalbox),label1,TRUE,TRUE,5);
+  label1 = Addlabel(verticalbox,"Escriba el nombre del archivo que quiere cargar:");
   
   entrybox = gtk_entry_new();
-
   Inicio->entry = entrybox;
-
   gtk_box_pack_start(GTK_BOX(verticalbox),entrybox,TRUE,TRUE,5);
-  gtk_signal_connect(GTK_OBJECT(windownombres),"destroy",GTK_SIGNAL_FUNC(StopTheApp),NULL);
 
-  button = AddButton1(windownombres,verticalbox,"Continuar",CARGAR,Inicio);
+  gtk_signal_connect(GTK_OBJECT(windownombres),"destroy",
+		     GTK_SIGNAL_FUNC(StopTheApp),NULL);
+
+  button = AddButton1(windownombres,verticalbox,"Continuar",
+		      CARGAR,Inicio);
 
   //Cuando se de click en continuar debe guardar lo que este en la entrybox
 
-  gtk_signal_connect(GTK_OBJECT(windownombres),"destroy",GTK_SIGNAL_FUNC(StopTheApp),NULL);
-  
   gtk_container_add(GTK_CONTAINER(windownombres),verticalbox);
-
   gtk_widget_show_all(windownombres);
-
-  gtk_widget_hide(windownombres);
-  //Cerrar ventana anterior
-  //gtk_widget_destroy(Movimientos->ventana);  
+  gtk_widget_hide(windownombres);  
 }
 
 void NOMBRES(GtkWidget *window, gpointer data){
@@ -239,79 +238,84 @@ void NOMBRES(GtkWidget *window, gpointer data){
   char text2[50],text3[50];
 
   Lista *Inicio=(Lista *)data;
-  GtkWidget *window2;
-  GtkWidget *row, *button, *box1;
-
-  //Guarda los nobres en la memoria
+  GtkWidget *window_tab;
+  GtkWidget *row_t, *box_t;
+  
+  //Guarda los nombres en la memoria
  
   text = gtk_entry_get_text(GTK_ENTRY(Inicio->Jugador1->entry));
   strcpy(text2, text);
   
-  text =gtk_entry_get_text(GTK_ENTRY(Inicio->Jugador2->entry));
+  text = gtk_entry_get_text(GTK_ENTRY(Inicio->Jugador2->entry));
   strcpy(text3,text);
+  
+  if(text2[0]=='\0' || text3[0]=='\0'){
+    quick_message("Escribe el nombre de ambos juagdores",Inicio->ventana);
+    return;
+  }
 
   if(text2[0]!='\0' && text3[0]!='\0'){ 
-    strcpy(Inicio->Jugador1->nombre,text2);
+    strcpy(Inicio->Jugador1->nombre, text2);
     strcpy(Inicio->Jugador2->nombre, text3);
   }
   
-  if(text2[0]!='\0' && text3[0]!='\0'){
-
-    if(strcmp(text2,text3)==0){
-      quick_message("Los nombres de los jugadores no pueden ser iguales",NULL);
+  if(Inicio->Jugador1->nombre[0]!='\0' && Inicio->Jugador2->nombre[0]!='\0'){
+    
+    if(strcmp(Inicio->Jugador1->nombre,Inicio->Jugador2->nombre)==0){
+      quick_message("Los nombres de los jugadores no pueden ser iguales",Inicio->ventana);
       return;
     }
     
-    strcpy(Inicio->Jugador1->nombre,text2);
-    strcpy(Inicio->Jugador2->nombre, text3);
+    window_tab = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     
-    window2 = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-
-    gtk_signal_connect(GTK_OBJECT(window2),"destroy",GTK_SIGNAL_FUNC(StopTheApp),NULL);
+    gtk_signal_connect(GTK_OBJECT(window_tab),"destroy",
+		       GTK_SIGNAL_FUNC(StopTheApp),NULL);
     
-    box1 = gtk_vbox_new(TRUE, 0);
+    box_t = gtk_vbox_new(TRUE, 0);
     
-    row = gtk_hbox_new(TRUE, 5);
-     
-    gtk_box_pack_start(GTK_BOX(box1), create_pad(Inicio), TRUE, FALSE, 0);
+    row_t = gtk_hbox_new(TRUE, 5);
     
-    g_signal_connect(window2, "delete-event", G_CALLBACK(delete_event), NULL);
-    g_signal_connect(window2, "destroy", G_CALLBACK(destroy), NULL);
+    gtk_box_pack_start(GTK_BOX(box_t), create_pad(Inicio), TRUE, FALSE, 0);
     
-    gtk_container_add(GTK_CONTAINER(window2), box1);
+    gtk_signal_connect(GTK_OBJECT(window_tab),"destroy",
+		     GTK_SIGNAL_FUNC(StopTheApp),NULL);
     
-    gtk_widget_show_all(window2);
-  
+    gtk_container_add(GTK_CONTAINER(window_tab), box_t);
+    
+    gtk_widget_show_all(window_tab);
+    
     gtk_main(); 
   }
+  
 }
 
 void quick_message (gchar *message, GtkWidget *parent) {
+  GtkWidget *window, *button, *box;
 
-   GtkWidget *dialog, *label;
+  window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_default_size(GTK_WINDOW(window),300,50);
+  gtk_container_border_width(GTK_CONTAINER(window),5);
+  
+  box = gtk_vbox_new(TRUE,0);
+  
+  button = gtk_button_new_with_label(message);
+  gtk_box_pack_start(GTK_BOX(box),button,FALSE,TRUE,10);
+  
+  gtk_signal_connect(GTK_OBJECT(button),"clicked",
+		     GTK_SIGNAL_FUNC(gtk_widget_hide_all),window);
+  
+  g_signal_connect_swapped(GTK_OBJECT(button),
+			   "clicked",
+			   G_CALLBACK(gtk_widget_show),
+			   GTK_OBJECT(parent));
+  gtk_widget_show(button);
 
-   dialog = gtk_dialog_new_with_buttons ("ERROR",
-                                         GTK_WINDOW (parent),
-                                         GTK_DIALOG_DESTROY_WITH_PARENT,
-                                         GTK_STOCK_OK,
-                                         GTK_RESPONSE_NONE,
-                                         NULL);
-   label = gtk_label_new (message);
-
-   g_signal_connect_swapped (GTK_OBJECT (dialog),
-                             "response",
-                             G_CALLBACK (gtk_widget_hide),
-                             GTK_OBJECT (dialog));
-   g_signal_connect_swapped (GTK_OBJECT (dialog),
-                             "response",
-                             G_CALLBACK (gtk_widget_show_all),
-                             GTK_OBJECT (parent));
-   
-   gtk_container_add (GTK_CONTAINER (GTK_DIALOG(dialog)->vbox),
-                      label);
-   gtk_widget_show_all (dialog);
+  gtk_signal_connect(GTK_OBJECT(window),"destroy",
+		       GTK_SIGNAL_FUNC(gtk_widget_show),parent);
+  
+  gtk_container_add (GTK_CONTAINER (window),box);
+  gtk_widget_show_all (window);
 }
-
 
 void CARGAR(GtkWidget *window, gpointer data){
   
@@ -374,21 +378,11 @@ void CARGAR(GtkWidget *window, gpointer data){
   }  
 }
 
-
 //---------------------------------------------------//***********************************************************//
 //---------------------------------------------------//***********************************************************//
 //---------------------------------------------------//***********************************************************//
 //---------------------------------------------------//***********************************************************//
 //---------------------------------------------------//***********************************************************//
-
-
-static gboolean delete_event(GtkWidget *widget, GdkEvent *event, gpointer *data){
-  return FALSE;
-}
-
-static void destroy(GtkWidget *widget, gpointer data) {
-  gtk_main_quit();
-}
 
 void command(int y) {
   
@@ -469,213 +463,182 @@ GtkWidget *create_pad(gpointer data) {
   return container;
 }
 
-/*
-int main(int argc, char **argv) {
-
-  GtkWidget *window;
-  GtkWidget *row, *button, *box1;
-  
-  gtk_init(&argc, &argv);
-  
-  window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-  
-  box1 = gtk_vbox_new(TRUE, 0);
-  
-  row = gtk_hbox_new(TRUE, 5);
-  
-  gtk_box_pack_start(GTK_BOX(box1), create_pad(), TRUE, FALSE, 0);
-  
-  g_signal_connect(window, "delete-event", G_CALLBACK(delete_event), NULL);
-  g_signal_connect(window, "destroy", G_CALLBACK(destroy), NULL);
-  
-  gtk_container_add(GTK_CONTAINER(window), box1);
-  
-  gtk_widget_show_all(window);
-  
-  gtk_main();
-  
-  return 0;
-}
-  */
-
-
-
+//arreglar oculatar mensahes
+//arreglar cerrar aplicacion desde tablero
 
 /*
-
-void ComeFichas(GtkWidget *widget, gpointer callback_data)
-{
-	int x, int y; //variables del tablero
-	var jugador=j; //j será el jugador en turno
-
-	//valida a la derecha
-	if(x<=17)
-	{	
-		if(casilla!="vacio" && casilla==j){
-			x++;
-			if(casilla!="vacio" && casilla!=j)
-			{
-				x++;
-				if(casilla!="vacio" && casilla!=j)
-				{
-					x++;
-					if(casilla!="vacio" && casilla==j)
-					{
-						j.comidas+2;
-						x--;
-						casilla=vacio;
-						x--;
-						casilla=vacio;
-					}
-				}
-			}
-		}
-	}
-
-	//valida diagonal abajo derecha
-	if(x<=17 && y<=17)
-	{
+void ComeFichas(GtkWidget *widget, gpointer callback_data){
+  int x, int y; //variables del tablero
+  var jugador=j; //j será el jugador en turno
+  
+  //valida a la derecha
+  if(x<=17)
+    {	
+      if(casilla!="vacio" && casilla==j){
+	x++;
+	if(casilla!="vacio" && casilla!=j)
+	  {
+	    x++;
+	    if(casilla!="vacio" && casilla!=j)
+	      {
+		x++;
 		if(casilla!="vacio" && casilla==j)
-		{
-			x++; 
-			y++;
-			if(casilla!="vacio" && casilla!=j)
-			{
-				x++;
-				y++;
-				if(casilla!="vacio" && casilla!=j)
-				{
-					x++;
-					y++;
-					if(casilla!="vacio" && casilla==j)
-					{
-						j.comidas+2;
-						x--;
-						casilla=vacia;
-						x--;
-						casilla=vacia;
-					}
-				}
-			}
-		}
-	}
-
-	//valida abajo
-	if(y<=17)
+		  {
+		    j.comidas+2;
+		    x--;
+		    casilla=vacio;
+		    x--;
+		    casilla=vacio;
+		  }
+	      }
+	  }
+      }
+    }
+  
+  //valida diagonal abajo derecha
+  if(x<=17 && y<=17)
+    {
+      if(casilla!="vacio" && casilla==j)
 	{
-		if(casilla!="vacio" && casilla==j)
+	  x++; 
+	  y++;
+	  if(casilla!="vacio" && casilla!=j)
+	    {
+	      x++;
+	      y++;
+	      if(casilla!="vacio" && casilla!=j)
 		{
-			y++;
-			if(casilla!="vacio" && casilla!=j)
-			{
-				y++;
-				if(casilla!="vacio" && casilla==j)
-				{
-					y++;
-					if(casilla!="vacio" && casilla!=j)
-					{
-						j.comidas+2;
-						y--;
-						casilla=vacia;
-						y--;
-						casilla=vacia;
-					}
-				}
-			}
+		  x++;
+		  y++;
+		  if(casilla!="vacio" && casilla==j)
+		    {
+		      j.comidas+2;
+		      x--;
+		      casilla=vacia;
+		      x--;
+		      casilla=vacia;
+		    }
 		}
+	    }
 	}
-
-	//valida diagonal abajo izquierda
-	if(x<=3 && y<=17)
+    }
+  
+  //valida abajo
+  if(y<=17)
+    {
+      if(casilla!="vacio" && casilla==j)
 	{
-		if(casilla!="vacio" && casilla==j)
+	  y++;
+	  if(casilla!="vacio" && casilla!=j)
+	    {
+	      y++;
+	      if(casilla!="vacio" && casilla==j)
 		{
-			x--; 
-			y++;
-			if(casilla!="vacio" && casilla!=j)
-			{
-				x--;
-				y++;
-				if(casilla!="vacio" && casilla!=j)
-				{
-					x--;
-					y++;
-					if(casilla!="vacio" && casilla==j)
-					{
-						j.comidas+2;
-						x--;
-						casilla=vacia;
-						x--;
-						casilla=vacia;
-					}
-				}
-			}
+		  y++;
+		  if(casilla!="vacio" && casilla!=j)
+		    {
+		      j.comidas+2;
+		      y--;
+		      casilla=vacia;
+		      y--;
+		      casilla=vacia;
+		    }
 		}
+	    }
 	}
+    }
+  
+  //valida diagonal abajo izquierda
+  if(x<=3 && y<=17)
+    {
+      if(casilla!="vacio" && casilla==j)
+	{
+	  x--; 
+	  y++;
+	  if(casilla!="vacio" && casilla!=j)
+	    {
+	      x--;
+	      y++;
+	      if(casilla!="vacio" && casilla!=j)
+		{
+		  x--;
+		  y++;
+		  if(casilla!="vacio" && casilla==j)
+		    {
+		      j.comidas+2;
+		      x--;
+		      casilla=vacia;
+		      x--;
+		      casilla=vacia;
+		    }
+		}
+	    }
+	}
+    }
 }
 
 
 void FilasDe4(GtkWidget *widget, gpointer callback_data)
 {
-	int x, y, i; //variables del tablero
-	var jugador=j; //j será el jugador en turno
-
-	//valida derecha
-	if(x<=17)
+  int x, y, i; //variables del tablero
+  var jugador=j; //j será el jugador en turno
+  
+  //valida derecha
+  if(x<=17)
+    {
+      while(casilla!="vacio" && casilla==j && i<=3)
 	{
-		while(casilla!="vacio" && casilla==j && i<=3)
-		{
-			i++;
-			x++;
-		}
-		if(i==3)
-		{
-			j.filas=j.filas++;
-		}
+	  i++;
+	  x++;
 	}
-
-	//valida diagonal derecha
-	if(x<=17 && y<=17)
+      if(i==3)
 	{
-		while(casilla!="vacio" && casilla==j && i<=3)
-		{
-			i++;
-			x++;
-			y++;
-		}
-		if(i==3)
-		{
-			j.filas=j.filas++;
-		}
+	  j.filas=j.filas++;
 	}
-
-	//valida abajo
-	if(y<=17)
+    }
+  
+  //valida diagonal derecha
+  if(x<=17 && y<=17)
+    {
+      while(casilla!="vacio" && casilla==j && i<=3)
 	{
-		while(casilla!="vacio" && casilla==j && i<=3)
-		{
-			i++;
-			y++;
-		}
-		if(i==3)
-		{
-			j.filas=j.filas++;
-		}
+	  i++;
+	  x++;
+	  y++;
 	}
-
-	//valida diagonal derecha abajo
-	if(x>3 && y<=17)
+      if(i==3)
 	{
-		while(casilla!="vacio" && casilla==j && i<=3)
-		{
-			i++;
-			x--;
-			y++;
-		}
-		if(i==3)
-		{
-			j.filas=j.filas++;
-		}
+	  j.filas=j.filas++;
 	}
+    }
+  
+  //valida abajo
+  if(y<=17)
+    {
+      while(casilla!="vacio" && casilla==j && i<=3)
+	{
+	  i++;
+	  y++;
+	}
+      if(i==3)
+	{
+	  j.filas=j.filas++;
+	}
+    }
+  
+  //valida diagonal derecha abajo
+  if(x>3 && y<=17)
+    {
+      while(casilla!="vacio" && casilla==j && i<=3)
+	{
+	  i++;
+	  x--;
+	  y++;
+	}
+      if(i==3)
+	{
+	  j.filas=j.filas++;
+	}
+    }
 }
 */
