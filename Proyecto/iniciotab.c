20890b4197dfc64dcf544b19fec8ad1013163589
MemoOR <dethkarz2009@hotmail.com>
2018-05-13 17:57:27 -0500

    game

--- Proyecto/iniciotab.c
+++ Proyecto/iniciotab.c
@@ -1,8 +1,16 @@
+/* @file pente.c
+*  @brief Este programa se encarga de crear la parte 
+*  gráfica de el juego de pente
+*  @author 
+*  @date 14/05/2018
+*/
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 #include<gtk/gtk.h>
 
+//estructura de jugador
 typedef struct _node{
   char nombre[20];
   GtkWidget *entry;
@@ -10,16 +18,19 @@ typedef struct _node{
   int comidas;
 }Jug;
 
+//estructura de turnos
 typedef struct _node3{
   struct _node3 *sig;
   int X;
   int Y;
 }Jugadas;
 
+//estructura de coordenadas en botones
 typedef struct _node4{
   int x,y;
 }Coordenadas;
 
+//estructura de datos generales
 typedef struct _node2{
   GtkWidget *entry,*entry2;
   GtkWidget *ventana,*label1,*label2,*labelt;
@@ -35,7 +46,7 @@ typedef struct _node2{
   int ganador;
 }Lista;
 
-
+//funciones de graficos//
 GtkWidget *AddButton(GtkWidget *theBox, const gchar *buttonText,gpointer CallBackFunction, Lista *EntryBox);
 GtkWidget *AddButton1(GtkWidget *window,GtkWidget *theBox,const gchar *buttonText, gpointer CallBackFunction,Lista *EntryBox);
 GtkWidget *Addlabel(GtkWidget *theBox, const gchar *buttonText);
@@ -44,22 +55,36 @@ void StopTheApp(GtkWidget *window, gpointer data);
 void Nuevo(GtkWidget *window, gpointer data);
 void Cargar(GtkWidget *window, gpointer data);
 void NOMBRES(GtkWidget *window, gpointer data);
+void quick_message (gchar *message, GtkWidget *parent);
+void close_window(GtkWidget *window, gpointer data);
+GtkWidget *create_pad();
+//funciones de graficos//
+
+//funciones de funcionamiento//
 void CARGAR(GtkWidget *window, gpointer data);
 void Cargarjugs(GtkWidget *window, gpointer data);
 void Guardar(GtkWidget *window, gpointer data);
 void command(int y);
 void callback(GtkWidget *widget, gpointer data);
-GtkWidget *create_pad();
-
 void ComeFichas(GtkWidget *widget, gpointer data);
 void FilasDe4(GtkWidget *widget, gpointer data);
-void quick_message (gchar *message, GtkWidget *parent);
-void close_window(GtkWidget *window, gpointer data);
 void Ganador(GtkWidget *window, gpointer data);
 void GUARDAR(GtkWidget *window, gpointer data);
+//funciones de funcionamiento//
 
 #include "funcion.h"
 
+/**
+*  Esta función recibe un caracter en el primer argumento y si
+*  este es una letra minuscula lo convierte a mayuscula y regresa 
+*  elnuevo valor en el segundo argumento.
+*  Regresa un 1 si pudo convertir a mayuscula el caracter
+*  y 0 si no pudo hacerlo.
+*  @author Iggy Pop
+*  @param chrData     El caracter a convertir (minuscula a mayuscula)
+*  @param *may        El caracter convertido en mayuscula
+*  @return int
+*/
 gint main ( gint argc, gchar *argv[]){
 
   GtkWidget *window1;
@@ -67,8 +92,6 @@ gint main ( gint argc, gchar *argv[]){
   GtkWidget *box,*label,*verticalbox;
   Lista *Inicio;
   Jug Jugador1,Jugador2;
-
-
   
   Inicio=(Lista*)malloc(sizeof(Lista));
   
@@ -93,11 +116,8 @@ gint main ( gint argc, gchar *argv[]){
   Inicio->X1=-1;
   Inicio->Y1=-1;
 
- 
 
-  //Ventana de nuevo juego o cargar
-
-  
+  //Ventana de nuevo juego o cargar  
   gtk_init(&argc, &argv);
 
   window1 = gtk_window_new (GTK_WINDOW_TOPLEVEL);
@@ -129,46 +149,121 @@ gint main ( gint argc, gchar *argv[]){
   return 0;
 }
 
+/**
+*  Esta función recibe los datos de un evento dentro de 
+*  una ventana y regresa un valor FALSE.
+*  @author Guillermo Ortega
+*  @param widget       La ventana
+*  @param event        El evento
+*  @param data         La estructura inicial
+*  @return gboolean
+*/
 static gboolean delete_event(GtkWidget *widget, GdkEvent *event, gpointer *data) {
   return FALSE;
 }
 
-GtkWidget *AddButton(GtkWidget *theBox, const gchar *buttonText,
-		     gpointer CallBackFunction, Lista *EntryBox){
+/**
+*  Esta función recibe los datos para crear un botón
+*  y regresa el botón creado.
+*  @author Guillermo Ortega
+*  @param *Box       Caja contenedora
+*  @param *Text      Texto del boton
+*  @param *CallBack  funcion a llamar
+*  @param *List      lista principal
+*  @return GtkButton
+*/
+GtkWidget *AddButton(GtkWidget *Box, const gchar *Text,
+		     gpointer CallBack, Lista *List){
   GtkWidget *button;
-  button = gtk_button_new_with_label(buttonText);
-  gtk_box_pack_start(GTK_BOX(theBox),button,FALSE,TRUE,10);
-  gtk_signal_connect(GTK_OBJECT(button),"clicked",GTK_SIGNAL_FUNC(CallBackFunction),EntryBox);
+  button = gtk_button_new_with_label(Text);
+  gtk_box_pack_start(GTK_BOX(Box),button,FALSE,TRUE,10);
+  gtk_signal_connect(GTK_OBJECT(button),"clicked",GTK_SIGNAL_FUNC(CallBack),List);
   gtk_widget_show(button);
   return button;
 }
 
-GtkWidget *AddButton1(GtkWidget *window, GtkWidget *theBox,const gchar *buttonText, gpointer CallBackFunction,Lista *EntryBox){
+/**
+*  Esta fucnión recibe los datos para crear un botón
+*  y regresa el botón creado.
+*  @author Guillermo Ortega
+*  @param *window    ventana que se va a ocultar
+*  @param *Box       Caja contenedora
+*  @param *Text      Texto del boton
+*  @param *CallBack  función a llamar
+*  @param *List      lista principal
+*  @return GtkButton
+*/
+GtkWidget *AddButton1(GtkWidget *window, GtkWidget *Box,
+		      const gchar *Text, gpointer CallBack,Lista *List){
   GtkWidget *button;
-  button = gtk_button_new_with_label(buttonText);
-  gtk_box_pack_start(GTK_BOX(theBox),button,FALSE,TRUE,10);
-  gtk_signal_connect(GTK_OBJECT(button),"clicked",GTK_SIGNAL_FUNC(CallBackFunction),EntryBox);
+  button = gtk_button_new_with_label(Text);
+  gtk_box_pack_start(GTK_BOX(Box),button,FALSE,TRUE,10);
+  gtk_signal_connect(GTK_OBJECT(button),"clicked",GTK_SIGNAL_FUNC(CallBack),Box);
   g_signal_connect_swapped (G_OBJECT (button), "clicked",G_CALLBACK (gtk_widget_hide), G_OBJECT (window));
   gtk_widget_show(button);
   return button;
 }
 
+/**
+*  Esta función recibe un caracter en el primer argumento y si
+*  este es una letra minuscula lo convierte a mayuscula y regresa 
+*  elnuevo valor en el segundo argumento.
+*  Regresa un 1 si pudo convertir a mayuscula el caracter
+*  y 0 si no pudo hacerlo.
+*  @author Guillermo Ortega
+*  @param chrData     El caracter a convertir (minuscula a mayuscula)
+*  @param *may        El caracter convertido en mayuscula
+*  @return int
+*/
 GtkWidget *Addlabel(GtkWidget *theBox, const gchar *buttonText){
-    GtkWidget *label;
-    label = gtk_label_new(buttonText);
-    gtk_box_pack_start(GTK_BOX(theBox),label,FALSE,FALSE,0);
-    gtk_widget_show(label);
-    return label;
+  GtkWidget *label;
+  label = gtk_label_new(buttonText);
+  gtk_box_pack_start(GTK_BOX(theBox),label,FALSE,FALSE,0);
+  gtk_widget_show(label);
+  return label;
 }
 
+/**
+*  Esta función recibe un caracter en el primer argumento y si
+*  este es una letra minuscula lo convierte a mayuscula y regresa 
+*  elnuevo valor en el segundo argumento.
+*  Regresa un 1 si pudo convertir a mayuscula el caracter
+*  y 0 si no pudo hacerlo.
+*  @author Iggy Pop
+*  @param chrData     El caracter a convertir (minuscula a mayuscula)
+*  @param *may        El caracter convertido en mayuscula
+*  @return int
+*/
 void close_window(GtkWidget *window, gpointer data){
   gtk_widget_destroy(window);
 }
 
+/**
+*  Esta función recibe un caracter en el primer argumento y si
+*  este es una letra minuscula lo convierte a mayuscula y regresa 
+*  elnuevo valor en el segundo argumento.
+*  Regresa un 1 si pudo convertir a mayuscula el caracter
+*  y 0 si no pudo hacerlo.
+*  @author Iggy Pop
+*  @param chrData     El caracter a convertir (minuscula a mayuscula)
+*  @param *may        El caracter convertido en mayuscula
+*  @return int
+*/
 void StopTheApp(GtkWidget *window, gpointer data){
   gtk_main_quit();
 }
 
+/**
+*  Esta función recibe un caracter en el primer argumento y si
+*  este es una letra minuscula lo convierte a mayuscula y regresa 
+*  elnuevo valor en el segundo argumento.
+*  Regresa un 1 si pudo convertir a mayuscula el caracter
+*  y 0 si no pudo hacerlo.
+*  @author Iggy Pop
+*  @param chrData     El caracter a convertir (minuscula a mayuscula)
+*  @param *may        El caracter convertido en mayuscula
+*  @return int
+*/
 void Nuevo(GtkWidget *window, gpointer data){
   GtkWidget *Entrybox;
   GtkWidget *windownombres;
@@ -216,6 +311,17 @@ void Nuevo(GtkWidget *window, gpointer data){
 
 /*********************************************************/
 
+/**
+*  Esta función recibe un caracter en el primer argumento y si
+*  este es una letra minuscula lo convierte a mayuscula y regresa 
+*  elnuevo valor en el segundo argumento.
+*  Regresa un 1 si pudo convertir a mayuscula el caracter
+*  y 0 si no pudo hacerlo.
+*  @author Iggy Pop
+*  @param chrData     El caracter a convertir (minuscula a mayuscula)
+*  @param *may        El caracter convertido en mayuscula
+*  @return int
+*/
 void Cargar(GtkWidget *window, gpointer data){
 
   GtkWidget *windownombres;
@@ -247,8 +353,18 @@ void Cargar(GtkWidget *window, gpointer data){
 
 }
 
+/**
+*  Esta función recibe un caracter en el primer argumento y si
+*  este es una letra minuscula lo convierte a mayuscula y regresa 
+*  elnuevo valor en el segundo argumento.
+*  Regresa un 1 si pudo convertir a mayuscula el caracter
+*  y 0 si no pudo hacerlo.
+*  @author Iggy Pop
+*  @param chrData     El caracter a convertir (minuscula a mayuscula)
+*  @param *may        El caracter convertido en mayuscula
+*  @return int
+*/
 void NOMBRES(GtkWidget *window, gpointer data){
-
   const gchar *text;
   char text2[50],text3[50];
 
@@ -256,10 +372,8 @@ void NOMBRES(GtkWidget *window, gpointer data){
   GtkWidget *window_tab,*verticalbox;
   GtkWidget *row_t, *box_t,*button,*box2,*box3,*box,*box4,*label,*label1,*label2,*bigbox;
   char turno[30], comid1[30], comid2[30];
-
   
   //Guarda los nombres en la memoria
- 
   text = gtk_entry_get_text(GTK_ENTRY(Inicio->Jugador1->entry));
   strcpy(text2, text);
   
@@ -293,12 +407,8 @@ void NOMBRES(GtkWidget *window, gpointer data){
     
     gtk_box_pack_start(GTK_BOX(box_t), create_pad(Inicio), FALSE, FALSE, 0);
 
-    verticalbox=gtk_vbox_new(TRUE,60);
-    
-    box = gtk_hbox_new(TRUE,50);
-    
-    button = AddButton1(window_tab,box,"Guardar",GUARDAR,Inicio);
-    
+    verticalbox=gtk_vbox_new(TRUE,30);
+  
     box2 = gtk_hbox_new(FALSE,50);
     strcpy(text2,"Turno de: ");
     strcat(text2,Inicio->Jugador1->nombre);
@@ -310,6 +420,7 @@ void NOMBRES(GtkWidget *window, gpointer data){
 
     strcpy(comid1,"Fichas comidas por ");
     strcat(comid1,Inicio->Jugador1->nombre);
+    strcat(comid1," :");
         
     box3 = gtk_hbox_new(FALSE,50);
     
@@ -319,16 +430,23 @@ void NOMBRES(GtkWidget *window, gpointer data){
     
     strcpy(comid2,"Fichas comidas por ");
     strcat(comid2,Inicio->Jugador2->nombre);
-
-    box4 = gtk_hbox_new(TRUE,50);
+    strcat(comid2," :");
+        
+    box4 = gtk_hbox_new(FALSE,50);
     label2 = Addlabel(box4,comid2);
     Inicio->label2 = label2;
     gtk_box_pack_start(GTK_BOX(verticalbox),box4,FALSE,FALSE,20);
 
-    
+    box = gtk_hbox_new(TRUE,10);
+    button = AddButton1(window_tab,box," Guardar ",GUARDAR,Inicio);
     gtk_box_pack_start(GTK_BOX(verticalbox),box,FALSE,FALSE,50);
-    //  gtk_box_pack_start(GTK_BOX(row_t),verticalbox,TRUE,TRUE,5);
-    gtk_box_pack_start(GTK_BOX(box_t),row_t,TRUE,TRUE,3);
+
+    box = gtk_hbox_new(TRUE,10);
+    button = AddButton1(window_tab,box,"<<<",NULL,Inicio);
+    button = AddButton1(window_tab,box,">>>",NULL,Inicio);
+    gtk_box_pack_start(GTK_BOX(verticalbox),box,FALSE,FALSE,5);
+    
+    //gtk_box_pack_start(GTK_BOX(row_t),verticalbox,TRUE,TRUE,5);
 
     bigbox=gtk_hbox_new(FALSE,10);
 
@@ -347,9 +465,19 @@ void NOMBRES(GtkWidget *window, gpointer data){
     
     gtk_main(); 
   }
-  
 }
 
+/**
+*  Esta función recibe un caracter en el primer argumento y si
+*  este es una letra minuscula lo convierte a mayuscula y regresa 
+*  elnuevo valor en el segundo argumento.
+*  Regresa un 1 si pudo convertir a mayuscula el caracter
+*  y 0 si no pudo hacerlo.
+*  @author Iggy Pop
+*  @param chrData     El caracter a convertir (minuscula a mayuscula)
+*  @param *may        El caracter convertido en mayuscula
+*  @return int
+*/
 void quick_message (gchar *message, GtkWidget *parent) {
   GtkWidget *window, *button, *box;
 
@@ -380,6 +508,17 @@ void quick_message (gchar *message, GtkWidget *parent) {
 //---------------------------------------------------//***********************************************************//
 //---------------------------------------------------//***********************************************************//
 
+/**
+*  Esta función recibe un caracter en el primer argumento y si
+*  este es una letra minuscula lo convierte a mayuscula y regresa 
+*  elnuevo valor en el segundo argumento.
+*  Regresa un 1 si pudo convertir a mayuscula el caracter
+*  y 0 si no pudo hacerlo.
+*  @author Iggy Pop
+*  @param chrData     El caracter a convertir (minuscula a mayuscula)
+*  @param *may        El caracter convertido en mayuscula
+*  @return int
+*/
 void command(int y) {
   
   switch(y){
@@ -399,6 +538,17 @@ void command(int y) {
   }
 }
 
+/**
+*  Esta función recibe un caracter en el primer argumento y si
+*  este es una letra minuscula lo convierte a mayuscula y regresa 
+*  elnuevo valor en el segundo argumento.
+*  Regresa un 1 si pudo convertir a mayuscula el caracter
+*  y 0 si no pudo hacerlo.
+*  @author Iggy Pop
+*  @param chrData     El caracter a convertir (minuscula a mayuscula)
+*  @param *may        El caracter convertido en mayuscula
+*  @return int
+*/
 void callback(GtkWidget *widget, gpointer data) {
   gchar *label;
   GdkPixbuf *pix1, *pix2;
@@ -406,10 +556,6 @@ void callback(GtkWidget *widget, gpointer data) {
 
   GtkWidget *button;
   char turn1[30],turn2[30];
-
-
-  
-
   
   if(Inicio->turno%2==1){
 
@@ -458,6 +604,13 @@ void callback(GtkWidget *widget, gpointer data) {
    g_free(label);
 }
 
+/**
+*  Esta función recibe la estructura con los datos generales
+*  y genera el tablero de juego.
+*  @author Guillermo Ortega
+*  @param data    La estructura general
+*  @return GtkWidget
+*/
 GtkWidget *create_pad(gpointer data) {
   Lista *Inicio=(Lista *)data;
   
@@ -487,19 +640,10 @@ GtkWidget *create_pad(gpointer data) {
       
       gtk_widget_set_size_request(button, 35, 35);
       g_signal_connect(button, "clicked", G_CALLBACK(callback), Inicio);
-
-      
+   
       gtk_box_pack_start(GTK_BOX(row), button, FALSE, TRUE, 2);
       gtk_widget_show(button);
     }
-  }
-
-  
+  }  
   return container;
 }
-
-//arreglar oculatar mensahes
-//arreglar cerrar aplicacion desde tablero
-
-
-
